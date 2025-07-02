a8a4fc2d4e48d0916e0d04a5c87dec661fc987e1
Maissa Chebbour <maissa.chebbour@gmail.com>
2025-07-02 10:27:58 +0000

    OPERATIONAL

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -12,6 +12,6 @@
      ,s.log_cost
      ,s.ship_cost
  FROM {{ref("int_orders_margin")}} o
- LEFT JOIN {{ref("stg_dbt__raw_gz_ship")}} s
+ LEFT JOIN  {{ref("stg_dbt__raw_gz_ship")}} s
      USING(orders_id)
  ORDER BY orders_id desc
